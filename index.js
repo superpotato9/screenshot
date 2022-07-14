63bca6edf742b94046b1457693670174d7f2c2ca
nathan <nkoliha@gmail.com>
2022-07-14 00:20:06 -0700

    added image and it updating via js
    did i style is...
    
    no
    
    🖕

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 // API for get requests
 fetchRes = fetch(
-  "https://owen-wilson-wow-api.herokuapp.com/wows/random");
+  "https:/superpotato9.com/tvdle/data");
 // fetchRes is the promise to resolve
 // it by using.then() method
 fetchRes.then(res =>
   res.json()).then(d => {
   //console.log(d)
   element = d
-  //document.getElementById("example").innerHTML =  element;
-  var iterator = element.values();
+  console.log(element["title"]);
+  release =  element["release"];
+  title = element['title'];
+  poster = element["poster"];
 
-  // Here all the e//lements of the array is being printed.
-  for (let elements of iterator) {
-    //console.log(elements);
-    first_object = elements
-  }
-  console.log(first_object);
+  console.log(release, title, poster);
+  document.getElementById("main").src=poster;
+
+  //document.getElementById("example").innerHTML =  element;
+
 
 
 
 })
+
+
