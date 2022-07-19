9589265c147a973c1ee7c586d3687b105200cceb
nathan <nkoliha@gmail.com>
2022-07-19 14:11:18 -0700

    added color block and figured out how to change image size with js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // API for get requests
 //TODO: remove the answers
+
 fetchRes = fetch(
   "https:/superpotato9.com/tvdle/data");
 // fetchRes is the promise to resolve
@@ -9,28 +10,40 @@ fetchRes.then(res =>
   //console.log(d)
   element = d
   console.log(element["title"]);
-  release =  element["release"];
+  release = element["release"];
   title = element['title'];
   poster = element["poster"];
+  answer = title;
 
-  console.log(release, title, poster);
-  document.getElementById("main").src=poster;
+
+  // console.log(release, title, poster);
+  document.getElementById("main").src = poster;
+
 
   //document.getElementById("example").innerHTML =  element;
 
+// document.getElementById('image-container').style.width = "30px";
 
+  document.getElementById("image-container").style.width = 10 + 'vw';
+  document.getElementById("image-container").style.height = 10 + 'vw';
 
 
 })
-
+chances = 6;
 function get_ans() {
   var user_answer = document.getElementById("answer").value;
-
+  if (chances == 0) {
+    console.log('u lose');
+  }
   console.log(user_answer)
+  if (user_answer == title) {
+    console.log('correct!')
+
+  } else {
+    chances = chances - 1;
+    console.log('wrong');
+
+  }
 }
 
-let box = document.querySelector('main');
-let width = box.offsetWidth;
-let height = box.offsetHeight;
-console.log(height);
-console.log(height);
+
