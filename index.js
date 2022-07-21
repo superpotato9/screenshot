267db47e2e528f0d241b965636266b2772a0706f
nathan <nkoliha@gmail.com>
2022-07-21 13:29:40 -0700

    switched question maek got centering to work better ui polish added modal for help

--- index.js
+++ index.js
@@ -38,12 +38,14 @@ let real_chances = 6;
 function get_ans() {
 
 
+
   var user_answer = document.getElementById("answer").value;
   if (user_answer == '') {
     user_answer = user_answer + '         '
   }
   if (correct == false) {
     if (chances == 0) {
+
       console.log('u lose');
       button.disabled = true
     }
@@ -100,4 +102,33 @@ function get_ans() {
     }
   }
   document.getElementById('answer').value = ''; //clears value of text box
+}
+
+function help() {
+  console.log('help ')
+  modal.style.display = "block";
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
